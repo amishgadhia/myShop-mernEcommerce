79c321d319fcfa2a97596915fe43b4b4f4ca50a7
Amish Gadhia <gadhiaamish@gmail.com>
2020-12-01 11:42:17 +0530

    added middleware and improve backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from "express";
-import products from "./data/products.js";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleWare.js";
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 
@@ -9,14 +10,11 @@ connectDB();
 
 const app = express();
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use("/api/products", productRoutes);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
